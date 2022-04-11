00a970b730ea3f6c3d70e6a849e638169527fe7a
kimha1030 <1478@holbertonschool.com>
2022-04-11 12:21:23 -0500

    Funciones taller1

--- taller1/figuras.js
+++ taller1/figuras.js
@@ -61,3 +61,48 @@ console.log(
 a_circulo = radio * radio * PI;
 console.log("El area del circulo es igual a " + Math.round(a_circulo) + " cm2");
 console.groupEnd();
+
+console.group("Funciones");
+// Funciones
+function perimetroCuadrado() {
+  const lado = document.getElementById("lado_cuadrado").value;
+  const p_cuadrado = lado * 4;
+  alert("El perimetro del cuadrado es igual a " + p_cuadrado + "cm");
+}
+
+function areaCuadrado() {
+  const lado = document.getElementById("lado_cuadrado").value;
+  const a_cuadrado = lado * lado;
+  alert("El area del cuadrado es igual a " + a_cuadrado + "cm2");
+}
+
+function perimetroTriangulo() {
+  const lado1 = document.getElementById("lado1_triangulo").value;
+  const lado2 = document.getElementById("lado2_triangulo").value;
+  const lado3 = document.getElementById("lado3_triangulo").value;
+  p_triangulo = parseInt(lado1) + parseInt(lado2) + parseInt(lado3);
+  alert("El perimetro del triangulo es igual a " + p_triangulo + "cm");
+}
+
+function areaTriangulo() {
+  const lado3 = document.getElementById("lado3_triangulo").value;
+  const altura = document.getElementById("altura_triangulo").value;
+  a_triangulo = (parseInt(altura) * parseInt(lado3)) / 2;
+  alert("El area del triangulo es igual a " + a_triangulo + "cm2");
+}
+
+function perimetroCirculo() {
+  const radio = document.getElementById("radio").value;
+  const diametro = parseInt(radio) * 2;
+  const PI = Math.PI;
+  p_circulo = diametro * PI;
+  alert("El perimetro del circulo es igual a " + Math.round(p_circulo) + " cm");
+}
+
+function areaCirculo() {
+  const radio = document.getElementById("radio").value;
+  const PI = Math.PI;
+  a_circulo = parseInt(radio) * parseInt(radio) * PI;
+  alert("El area del circulo es igual a " + Math.round(a_circulo) + " cm2");
+}
+console.groupEnd();
